3c20c0ba00a3a47489e7acfd9c37cefe39995856
Abinash Singh <abinash1@ualberta.ca>
2023-05-22 11:04:36 -0600

    Removed X11 header

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include <X11/X.h>
+// #include <X11/X.h>
 #include <iostream>
 #include <stdio.h>
 #define GL_SILENCE_DEPRECATION
